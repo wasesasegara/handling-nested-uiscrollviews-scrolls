430098a57829b12d607212431c6b0fc61573e2c7
wasesasegara <wasesasegara@gmail.com>
2020-08-05 22:03:42 +0700

    init: Project initiation

---
+++ handling-nested-uiscrollviews-scrolls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  handling-nested-uiscrollviews-scrolls
+//
+//  Created by Bisma S Wasesasegara on 05/08/20.
+//  Copyright © 2020 Bisma S Wasesasegara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
