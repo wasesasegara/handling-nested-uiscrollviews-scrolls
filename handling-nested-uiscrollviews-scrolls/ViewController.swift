b6071307a22cd4061c40273b74f3ac8f65df5ae8
wasesasegara <wasesasegara@gmail.com>
2020-08-05 22:43:12 +0700

    feat: Init collection view
    Programmatically created

--- handling-nested-uiscrollviews-scrolls/ViewController.swift
+++ handling-nested-uiscrollviews-scrolls/ViewController.swift
@@ -9,12 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupCollectionView()
     }
-
-
+    
+    fileprivate func setupCollectionView() {
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: String(describing: CollectionViewCell.self))
+        collectionView.isPagingEnabled = true
+        
+        // MARK: collectionView flow layout
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = view.frame.size
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.scrollDirection = .horizontal
+        
+        collectionView.collectionViewLayout = layout
+    }
 }
 
+// MARK: - ViewController collection view delegate
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+// MARK: - ViewCOntroller collection view data source
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: CollectionViewCell.self), for: indexPath)
+        return cell
+    }
+}
+
+// MARK: -
+class CollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func create(content: UIView?) {
+        guard let content = content else { return }
+        addSubview(content)
+        setupConstraints(content)
+    }
+    
+    fileprivate func setupConstraints(_ content: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        content.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        content.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        content.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        content.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+    }
+}
